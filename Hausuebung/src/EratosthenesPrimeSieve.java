2103300bdcf11749be0d6c25e817e9daa081fff7
Shuajb Ramadani <RamadaniS190231@sus.htl-grieskirchen.at>
2022-09-22 10:53:56 +0200

    Changed some things

--- Hausuebung/src/EratosthenesPrimeSieve.java
+++ Hausuebung/src/EratosthenesPrimeSieve.java
@@ -7,5 +7,15 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
     }
 
 
+    @Override
+    public boolean isPrime(int p) {
 
+
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
 }
--- Hausuebung/src/Main.java
+++ Hausuebung/src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Bis wo wollen Sie die Primzahlen ausrechen");
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
+        int highestNumber = sc.nextInt();
+        int[] crossed = new int[highestNumber];
+        
+        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(highestNumber);
+
+
+        for(int i = 2; i == highestNumber; i++){
+
+        }
     }
 }
