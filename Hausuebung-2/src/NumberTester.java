5b91b9785cf3a5be4bea566a0fd67d370fd82bec
Shuajb Ramadani <RamadaniS190231@sus.htl-grieskirchen.at>
2022-09-29 10:16:16 +0200

    NumberTester class created, NumberTest interface created

---
+++ Hausuebung-2/src/NumberTester.java
@@ -0,0 +1,26 @@
+public class NumberTester {
+    private NumberTester primeTester;
+    private NumberTester oddTester;
+    private NumberTester palindromeTester;
+
+    public NumberTester(String fileName){
+        //TODO
+    }
+
+    public void setOddEvenTester(NumberTester oddTester){
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTester primeTester){
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTester palindromeTester){
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile(){
+        //TODO
+    }
+
+}
