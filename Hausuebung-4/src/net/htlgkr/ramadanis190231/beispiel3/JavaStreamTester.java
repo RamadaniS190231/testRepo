f0d195be5ab059591668b8dd05a91335e547a070
Shuajb Ramadani <RamadaniS190231@sus.htl-grieskirchen.at>
2022-10-19 18:38:33 +0200

    Hausübung fertig

--- Hausuebung-4/src/net/htlgkr/ramadanis190231/beispiel3/JavaStreamTester.java
+++ Hausuebung-4/src/net/htlgkr/ramadanis190231/beispiel3/JavaStreamTester.java
@@ -9,15 +9,24 @@ public class JavaStreamTester {
 
     public static void main(String[] args) {
         List<String> arrStr1 = Arrays.asList("abc", "", "bc", "Hallo", "", "", "jkl");
-        List<Integer> arrStr2 = Arrays.asList(1,2,3,4,5,6);
+        List<Integer> arrStr2 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> arrStr3 = Arrays.asList(-12, -6, 0, 5, 13);
+        List<Integer> arrStr4 = Arrays.asList(2, 3, 4, 5, 6);
+
         System.out.println(getCountEmptyString(arrStr1));
         System.out.println(getCountLength3(arrStr1));
         System.out.println(deleteEmptyStrings(arrStr1));
         System.out.println(getMergedString(arrStr1, ":"));
         System.out.println(getSquares(arrStr2));
+        System.out.println(getMax(arrStr3));
+        System.out.println(getMin(arrStr3));
+        System.out.println(getSum(arrStr4));
+        System.out.println(getAverage(arrStr4));
+
+
     }
 
-    private static int getCountEmptyString(List<String> strings){
+    private static int getCountEmptyString(List<String> strings) {
         long emptyStrings = strings
                 .stream()
                 .filter(x -> x.isEmpty())
@@ -25,7 +34,7 @@ public class JavaStreamTester {
         return (int) emptyStrings;
     }
 
-    private static int getCountLength3(List<String> strings){
+    private static int getCountLength3(List<String> strings) {
         long lengthString = strings
                 .stream()
                 .filter(x -> x.length() == 3)
@@ -33,7 +42,7 @@ public class JavaStreamTester {
         return (int) lengthString;
     }
 
-    private static List<String> deleteEmptyStrings(List<String> strings){
+    private static List<String> deleteEmptyStrings(List<String> strings) {
         List<String> list = strings
                 .stream()
                 .filter(x -> !x.equals(""))
@@ -41,34 +50,46 @@ public class JavaStreamTester {
         return list;
     }
 
-    private static String getMergedString(List<String> strings, String seperator){
+    private static String getMergedString(List<String> strings, String seperator) {
         String s = strings
                 .stream()
                 .collect(Collectors.joining(seperator));
         return s;
     }
 
-    private static List<Integer> getSquares(List<Integer> numbers){
+    private static List<Integer> getSquares(List<Integer> numbers) {
         List<Integer> list = numbers
                 .stream()
-                .map(n -> n*n)
+                .map(n -> n * n)
                 .collect(Collectors.toList());
         return list;
     }
 
-    private static int getMax(List<Integer> numbers){
-        return 0;
+    private static int getMax(List<Integer> numbers) {
+
+        int max = numbers
+                .stream()
+                .max(Integer::compareTo)
+                .get();
+        return max;
     }
 
-    private static int getMin(List<Integer> numbers){
-        return 0;
+    private static int getMin(List<Integer> numbers) {
+
+        int min = numbers
+                .stream()
+                .min(Integer::compareTo)
+                .get();
+        return min;
     }
 
-    private static int getSum(List<Integer> numbers){
-        return 0;
+    private static int getSum(List<Integer> numbers) {
+        int sumResult = numbers.stream().mapToInt(n -> n).sum();
+        return sumResult;
     }
 
-    private static int getAverage(List<Integer> numbers){
-        return 0;
+    private static int getAverage(List<Integer> numbers) {
+        double avrg = numbers.stream().mapToInt(n -> n).average().getAsDouble();
+        return (int) avrg;
     }
 }
