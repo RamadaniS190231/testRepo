cca42b805f68fb3cb180d2f55fde0d821380ca3c
Shuajb Ramadani <RamadaniS190231@sus.htl-grieskirchen.at>
2022-10-19 10:18:21 +0200

    Aufgabe 4 - Beispiel 3, Methoden erstellt

---
+++ Hausuebung-4/src/net/htlgkr/ramadanis190231/beispiel3/JavaStreamTester.java
@@ -0,0 +1,46 @@
+package net.htlgkr.ramadanis190231.beispiel3;
+
+import java.util.List;
+
+public class JavaStreamTester {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int getCountEmptyString(List<String> strings){
+        return 0;
+    }
+
+    private static int getCountLength3(List<String> strings){
+        return 0;
+    }
+
+    private static List<String> deleteEmptyStrings(List<String> strings){
+        return null;
+    }
+
+    private static String getMergedString(List<String> strings, String seperator){
+        return "";
+    }
+
+    private static List<Integer> getSquares(List<Integer> numbers){
+        return null;
+    }
+
+    private static int getMax(List<Integer> numbers){
+        return 0;
+    }
+
+    private static int getMin(List<Integer> numbers){
+        return 0;
+    }
+
+    private static int getSum(List<Integer> numbers){
+        return 0;
+    }
+
+    private static int getAverage(List<Integer> numbers){
+        return 0;
+    }
+}
